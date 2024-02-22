e9f8698abbed2332087fc7ff72c95ec3662c4428
Laura Elena Mesa <mesafernandezlauraelena@gmail.com>
2024-02-22 17:12:23 -0500

    Starting the projects

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,52 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom';
+import { Home } from '../Home/Home';
+import { MyAccount } from '../MyAccount/MyAccount';
+import { MyOrder } from '../MyOrder/MyOrder';
+import { MyOrders } from '../MyOrders/MyOrders';
+import { NotFound } from '../NotFound/NotFound';
+import { SignIn } from '../SignIn/SignIn';
+
+
+import './App.css';
+
+const AppRoutes = () => {
+  let routes = useRoutes([
+    {
+      path: '/',
+      element: <Home/>
+    },
+    {
+      path: '/my-account',
+      element: <MyAccount/>
+    },
+    {
+      path: '/my-order',
+      element: <MyOrder/>
+    },
+    {
+      path: '/my-orders',
+      element: <MyOrders/>
+    },
+    {
+      path: '/sign-in',
+      element: <SignIn/>
+    },
+    {
+      path: '/*',
+      element: <NotFound/>
+    },
+  ])
+
+  return routes;
+}
+
+const App = () => {
+
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  )
+}
+
+export default App
