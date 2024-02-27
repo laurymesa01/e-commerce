bbcd2a57e3dabdb1ed0efa7377b466a9db0bcb19
Laura Elena Mesa <mesafernandezlauraelena@gmail.com>
2024-02-27 10:48:58 -0500

    Adding icons to the project

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -15,7 +15,10 @@ const Card = (props) => {
                 <button 
                     className="absolute top-0 right-0 flex justify-center items-center bg-white hover:bg-gray-200 text-black w-6 h-6 rounded-full m-2 p-1"
                     onClick= { () => setCounter(counter + 1) }>
-                    +
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                    </svg>
+
                 </button>
             </figure>
             <p className="flex justify-between">
--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -5,7 +5,12 @@ const ProductDetail = () => {
         <aside className="product-detail flex flex-col fixed right-0 top-[68px] border border-black rounded bg-white w-[360px] h-[calc(100vh-60px)]">
             <div className='flex justify-between items-center p-6'>
                 <h2 className='font-medium text-xl'>Detail</h2>
-                <div>x</div>
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 cursor-pointer">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                    </svg>
+
+                </div>
             </div>
         </aside>
     )
